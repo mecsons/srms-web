aac12a1c621a8ee3910947431cd8673de0feef1e
Ringo Ebenezer <ringo.ebenezer@gmail.com>
2026-01-23 14:11:34 +0300

    results coverage is not needed

--- src/routes/_protected/academics/assessments/$assessmentId.tsx
+++ src/routes/_protected/academics/assessments/$assessmentId.tsx
@@ -26,11 +26,10 @@ function RouteComponent() {
             <PageTitle title={assessment.name}/>
 
             <div className={"flex flex-col gap-7"}>
-                <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                     <StatCard title={"Academic Year"} value={"2025"}/>
                     <StatCard title={"Duration"} value={"Jan 15, 2026 → Jan 29, 2026"}/>
                     <StatCard title={"Scope"} value={"3 grades • 8 subjects"}/>
-                    <StatCard title={"Results Coverage"} value={"412 / 520 students entered"}/>
                 </div>
 
                 <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
